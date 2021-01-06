6b10b1532819637709159b5298a6ccbcdf5e8fe3
Austin Lane Hearns Howard <austin.lane.howard@gmail.com>
2021-01-06 12:12:30 -0700

    Initial commit

---
+++ MUD-Space.cpp
@@ -0,0 +1,12 @@
+﻿// MUD-Space.cpp : Defines the entry point for the application.
+//
+
+#include "MUD-Space.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
